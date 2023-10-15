990070baf8383c72a09d6537b9eb4e3679bf616e
assiaamahouch <assia.amahouch@gmail.com>
2023-10-15 21:20:31 +0200

    jhb

--- bank/MyForm.h
+++ bank/MyForm.h
@@ -43,6 +43,7 @@ namespace bank {
 	private: System::Windows::Forms::Panel^ pn_client;
 
 	private: System::Windows::Forms::Panel^ principal_panel;
+	private: System::Windows::Forms::Button^ button1;
 
 
 
@@ -66,52 +67,69 @@ namespace bank {
 			this->pn_admin = (gcnew System::Windows::Forms::Panel());
 			this->pn_client = (gcnew System::Windows::Forms::Panel());
 			this->principal_panel = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->principal_panel->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// pn_login
 			// 
 			this->pn_login->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->pn_login->Location = System::Drawing::Point(0, 0);
+			this->pn_login->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->pn_login->Name = L"pn_login";
-			this->pn_login->Size = System::Drawing::Size(854, 427);
+			this->pn_login->Size = System::Drawing::Size(1518, 662);
 			this->pn_login->TabIndex = 0;
 			// 
 			// pn_admin
 			// 
 			this->pn_admin->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->pn_admin->Location = System::Drawing::Point(0, 0);
+			this->pn_admin->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->pn_admin->Name = L"pn_admin";
-			this->pn_admin->Size = System::Drawing::Size(854, 427);
+			this->pn_admin->Size = System::Drawing::Size(1518, 662);
 			this->pn_admin->TabIndex = 1;
 			// 
 			// pn_client
 			// 
 			this->pn_client->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->pn_client->Location = System::Drawing::Point(0, 0);
+			this->pn_client->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->pn_client->Name = L"pn_client";
-			this->pn_client->Size = System::Drawing::Size(854, 427);
+			this->pn_client->Size = System::Drawing::Size(1518, 662);
 			this->pn_client->TabIndex = 2;
 			// 
 			// principal_panel
 			// 
+			this->principal_panel->Controls->Add(this->button1);
 			this->principal_panel->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->principal_panel->Location = System::Drawing::Point(0, 0);
+			this->principal_panel->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->principal_panel->Name = L"principal_panel";
-			this->principal_panel->Size = System::Drawing::Size(854, 427);
+			this->principal_panel->Size = System::Drawing::Size(1518, 662);
 			this->principal_panel->TabIndex = 3;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(516, 199);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(316, 184);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(854, 427);
+			this->ClientSize = System::Drawing::Size(1518, 662);
 			this->Controls->Add(this->principal_panel);
 			this->Controls->Add(this->pn_client);
 			this->Controls->Add(this->pn_admin);
 			this->Controls->Add(this->pn_login);
-			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
+			this->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->principal_panel->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
